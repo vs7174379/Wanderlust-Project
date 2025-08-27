a0f831051486ced322888645dbfa405797c9f5c4
delta-student <136906596+vs7174379@users.noreply.github.com>
2025-08-27 11:50:45 +0530

    commit

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,5 +1,6 @@
 const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary-v2');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
 // ✅ Configure Cloudinary with environment variables
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
